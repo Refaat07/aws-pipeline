eee29ec9341e109fc2b62491279556705c35b7d1
Mohamed Refaat <47618425+Refaat07@users.noreply.github.com>
2024-05-08 22:42:42 +0300

    Update tf code

--- Terraform/role.tf
+++ Terraform/role.tf
@@ -27,7 +27,7 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution_policy" {
 
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = "terraform-mrefaat"
+  bucket = "iti-terraform-cloud-pd44"
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.send_email_lambda.arn
@@ -40,5 +40,5 @@ resource "aws_lambda_permission" "allow_bucket" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.send_email_lambda.function_name
   principal     = "s3.amazonaws.com"
-  source_arn    = "arn:aws:s3:::terraform-mrefaat"
+  source_arn    = "arn:aws:s3:::iti-terraform-cloud-pd44"
 }
